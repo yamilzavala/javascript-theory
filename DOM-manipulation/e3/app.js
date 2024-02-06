9f9badb6c1d2180c56c4a168da0c3aff69f9af18
yamilzavala <zavalayamil9@gmail.com>
2024-02-06 10:59:08 -0300

    dom manipulation

--- DOM-manipulation/e3/app.js
+++ DOM-manipulation/e3/app.js
@@ -36,3 +36,35 @@ pEl.style.fontSize = '40px'
 
 //classes
 firstLi.classList.toggle('li-remove')
+
+//creating elements
+const newH2 = document.createElement('h2')
+newH2.innerText = 'Im new'
+newH2.classList.add('special')
+const sec = document.querySelector('section');
+sec.appendChild(newH2)
+
+const newImg = document.createElement('img');
+newImg.src = 'https://images.pexels.com/photos/19845816/pexels-photo-19845816/free-photo-of-aerial-view-of-a-river-with-snow-on-the-surface.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load';
+document.body.appendChild(newImg)
+newImg.style.width = '200px'
+newImg.style.height = '200px'
+
+const newLink = document.createElement('a')
+newLink.innerText = 'go img'
+newLink.href = 'https://images.pexels.com/photos/19845816/pexels-photo-19845816/free-photo-of-aerial-view-of-a-river-with-snow-on-the-surface.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+sec.appendChild(newLink)
+
+const newLi = document.createElement('li')
+newLi.innerText = 'New li';
+ulEl.insertBefore(newLi, ulEl.firstChild)
+
+const newI = document.createElement('i')
+newI.innerText = 'New italic'
+pEl.prepend(newI, newLink)
+
+const liElremoveMe = document.querySelector('ul li.target-class')
+liElremoveMe.remove();
+
+const firslsLiToRemove = document.querySelector('ul li');
+ulEl.removeChild(firslsLiToRemove)
